85c244e7c85097608596c1293679fb8ce3f7248a
Manoj Kumar Dash <manoj.dash@mitel.com>
2023-07-19 01:43:26 +0530

    login

--- app/Controllers/WebController.php
+++ app/Controllers/WebController.php
@@ -10,37 +10,18 @@ class WebController extends BaseController {
     public function __construct() {
         parent::__construct();
         $this->webModel = new WebModel();
-//        $openmethods = array('generatePrescriptionEmail', 'deleteSystemLogs', 'deleteEmailAttachment', 'generateInvoiceEmail', 'autocancellPendingBooking', 'sendPendingEmails', 'checkMobileExist', 'checkEmailExist', 'homepage', 'termsandcondition', 'privacypolicy', 'aboutus', 'contactus', 'searchdoctor', 'doctorprofile', 'login', 'logout', 'registration', 'forgotpassord', 'resetpassord');
-//        $othermemdercanaccess = array('uploadmedicalReport', 'prescriptionprint','prescriptionview','viewmedicalReport');
-//        if (!in_array($this->method, $openmethods)) {
-//            if ($this->session->has('login')) {
-//                if (!in_array($this->method, $othermemdercanaccess)) {
-//                    if ($this->session->get('usertype') != 3) {
-//                        header("location:/login");
-//                        exit;
-//                    }
-//                }
-//            } else {
-//                header("location:/login");
-//                exit;
-//            }
-//        }
+        $openmethods = array('deleteSystemLogs', 'deleteEmailAttachment', 'login', 'logout', 'forgotpassord', 'resetpassord');
+        if (!in_array($this->method, $openmethods)) {
+            if ($this->session->has('login')) {                
+            } else {
+                header("location:/login");
+                exit;
+            }
+        }
         
     }
 
-    public function sendPendingEmails() {
-        $queuedemail = $this->webModel->getQueuedEmail(5);
-        foreach ($queuedemail as $email) {
-            $emaildata = array('template' => $email->smtp_email_content, 'to' => $email->smtp_target_emails, 'subject' => $email->smtp_email_type);
-            (!empty($email->smtp_attachment)) ? $emaildata['attachment'][] = __DIR__ . '/../../public/uploads/emailattachments/' . $email->smtp_attachment : '';
-            $status = sendEmail($emaildata);
-            if ($status) {
-                $updarray = array('smtp_send_status' => 1, 'smtp_deliver_date_time' => date('Y-m-d H:i:s'));
-                $this->blankModel->updateRecordInTable($updarray, 'smtp_email', 'smtp_send_id', $email->smtp_send_id);
-            }
-        }
-    }
-
+    
     //deleteing the logfile older than 7 days--- 
     public function deleteSystemLogs() {
         try {
