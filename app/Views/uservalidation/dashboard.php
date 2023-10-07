6293a795f03b6ae6faa1da132a9a7f6964ca704f
Manoj Kumar Dash <manojdashmca@gmail.com>
2023-10-07 20:07:18 +0530

    progress

--- app/Views/uservalidation/dashboard.php
+++ app/Views/uservalidation/dashboard.php
@@ -13,41 +13,56 @@
                 });
         }
     }
-    <?php
-    $cities=array();
-    $contractcost=array();
-    $achievedprogress_percentage=array();
-    for($x=0;$x<count($financialdata);$x++){
-       $cities[]= $financialdata[$x]->city_name;
-       $contractcost[]= (int)$financialdata[$x]->contract_cost;
-       $achievedprogress[]= (int)$financialdata[$x]->achievedprogress;
-    }
-    
-    ?>
-
+<?php
+$cities = array();
+$cityid = array();
+$contractcost = array();
+$achievedprogress = array();
+for ($x = 0; $x < count($financialdata); $x++) {
+    $cities[] = $financialdata[$x]->city_name;
+    $cityid[] = $financialdata[$x]->city_id;
+    $contractcost[] = (int) $financialdata[$x]->contract_cost;
+    $achievedprogress[] = (int) $financialdata[$x]->achievedprogress;
+}
+?>
+    var cityids =<?= json_encode($cityid) ?>;
+    console.log(cityids);
     var chartColumnColors = getChartColorsArray("customer_impression_charts1"),
             chartColumnDatatalabelColors =
             (chartColumnColors &&
                     ((options = {
-                        chart: {height: 350, type: "bar", toolbar: {show: !1}},
+                        chart: {
+                            height: 350,
+                            type: "bar",
+                            toolbar: {show: !1},
+                            events: {
+                                dataPointSelection: function (event, chartContext, opts) {
+//                                    console.log(event);
+//                                    console.log(chartContext);
+//                                    console.log(opts);
+                                    window.open("<?= base_url() ?>/physical-progress?pager=<?= $pagerid ?>&city=" + cityids[opts.dataPointIndex]+"&transactionid=<?= $session->get('trnid') ?>");
+
+                                }
+                            }
+                        },
                         plotOptions: {bar: {dataLabels: {position: "top"}}},
                         dataLabels: {
                             enabled: !0,
                             formatter: function (t) {
-                                return t ;
+                                return t;
                             },
                             offsetY: -20,
                             style: {fontSize: "12px", colors: ["#adb5bd"]},
                         },
                         //series: [{name: "Progress", data: [2.5, 3.2, 5, 10.1, 4.2, 3.8, 3, 2.4, 4, 1.2, 3.5, 0.8, 18.7, 20.9]}],
                         series: [
-                            {name: "Contract Cost", data: <?=json_encode($contractcost)?>},
-                            {name: "Achieved Progress", data: <?=json_encode($achievedprogress)?>},
+                            {name: "Contract Cost", data: <?= json_encode($contractcost) ?>},
+                            {name: "Achieved Progress", data: <?= json_encode($achievedprogress) ?>},
                         ],
                         colors: chartColumnDatatalabelColors,
                         grid: {borderColor: "#f1f1f1"},
-                        xaxis: {categories: <?=json_encode($cities)?>},
-                   
+                        xaxis: {categories: <?= json_encode($cities) ?>},
+
                         fill: {gradient: {shade: "light", type: "horizontal", shadeIntensity: 0.25, gradientToColors: void 0, inverseColors: !0, opacityFrom: 1, opacityTo: 1, stops: [50, 0, 100, 100]}},
                         yaxis: {
                             axisBorder: {show: !1},
@@ -55,11 +70,11 @@
                             labels: {
                                 show: !1,
                                 formatter: function (t) {
-                                    return t + "%";
+                                    return t;
                                 },
                             },
                         },
-                        
+
                     }),
                             (chart = new ApexCharts(document.querySelector("#customer_impression_charts1"), options)).render()));
 
