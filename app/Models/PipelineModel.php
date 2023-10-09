096c5e442d04676f59dc9304619896467f840c2c
Chittaranjan Mohnata <141301764+Chittta@users.noreply.github.com>
2023-10-09 15:53:23 +0530

    remove print option on getAllStatedataMaster

--- app/Models/PipelineModel.php
+++ app/Models/PipelineModel.php
@@ -549,33 +549,8 @@ class PipelineModel extends Model
             $sql .= "WHERE dm.city_id = '$city';";
          }
         
-        // COUNT(DISTINCT(dm.city_id)) AS total_cities, 
-        
-
-        // "
-        // $sql = "SELECT SUM(z.population) AS total_no_population, 
-        // SUM(z.no_house_holds) AS total_no_house_holds, 
-        // SUM(z.no_house_coction) AS total_no_house_coction, 
-        // SUM(z.no_house_connection_replaced) AS total_no_house_connection_replaced, 
-        // SUM(z.no_metered_house_connections) AS total_no_metered_house_connections, 
-        // COUNT(DISTINCT(z.division_id)) AS total_division, 
-        // COUNT(DISTINCT(z.city_id)) AS total_cities, 
-        // COUNT(DISTINCT(z.id)) AS total_dma,
-        // (SELECT COUNT(id) FROM `jalsathi_word`) as total_jalasathi,
-        // ROUND(AVG(nrw), 2) AS nrw_average_value,
-        // (SELECT SUM(population) FROM zone_master WHERE no_house_holds = no_house_coction AND no_house_holds = no_metered_house_connections) AS beneficiary_population 
-        //  FROM zone_master z ";
-        //  if($division && $city == ""){
-        //     $sql .= "WHERE z.division_id = '$division';";
-            
-        //  }else if($city != "" && $division != ""){
-        //     $sql .= "WHERE z.city_id = '$city';";
-        //  }
-
         $result = $this->db->query($sql);
         $return = $result->getResult();
-        print_r($return);
-
         return $return;
     }
     public function getJalsathiOnDivisionAndCity($division,$city)
