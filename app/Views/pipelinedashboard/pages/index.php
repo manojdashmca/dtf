b70bccacd0a5d95f822a966ed4a7733b485895e3
Chittaranjan Mohnata <141301764+Chittta@users.noreply.github.com>
2023-11-10 10:55:16 +0530

    conflit dashboard2

--- app/Views/pipelinedashboard/pages/index.php
+++ app/Views/pipelinedashboard/pages/index.php
@@ -2164,8 +2164,7 @@ circle[data-slide-to="0"] {
                     </svg>
 
                     <div class="construction-progress">
-                        <h5><img src="images/icon/progress.png" alt=""> Construction Progress: <span
-                                class="text-info">45%</span></h5>
+                        
 						<a href="/dashboard">
                         	<h5><img src="images/icon/progress.png" alt=""> Construction Progress: <span class="text-info">45%</span></h5>
 						</a>
